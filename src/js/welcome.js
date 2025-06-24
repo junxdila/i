89a049267e3e7b0babd121a715926724dc29bdd7
junxdila <tikusktq@gmail.com>
2025-06-24 18:16:02 +0700

    Update welcome.js

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -24,10 +24,10 @@ export const welcome = () => {
         const params = getQueryParameter('to');
 
         if (params) {
-            weddingToElement.innerHTML = `Kepada Yth Bapak/Ibu/Saudara/(i)<br><span>${params}</span>`;
+            weddingToElement.innerHTML = `<i>Kepada Yth Bapak/Ibu/Saudara/(i)</i><br><span>${params}</span>`;
             name.value = params;
         } else {
-            weddingToElement.innerHTML = `Kepada Yth Bapak/Ibu/Saudara/(i)<br><span>Teman-teman semua</span>`;
+            weddingToElement.innerHTML = `<i>Kepada Yth Bapak/Ibu/Saudara/(i)</i><br><span>Teman-teman semua</span>`;
         }
     }
 
